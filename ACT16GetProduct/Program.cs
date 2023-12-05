1212b6f52f295da2c0813c18f16ed40aacecaa40
MarcosGorriaran <Marcos.Gorriaran.7e7@itb.cat>
2023-12-05 12:55:37 +0100

    Actividad 15 en progreso

--- ACT16GetProduct/Program.cs
+++ ACT16GetProduct/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+namespace ACT15
+{
+    public class ACT15
+    {
+        public static void Main()
+        {
+            
+        }
+        public static bool InRange(int)
+        {
+
+        }
+    }
+}
