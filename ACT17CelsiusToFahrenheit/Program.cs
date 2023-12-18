a76650df334cb36e8263258f90a260705413a0c8
MarcosGorriaran <Marcos.Gorriaran.7e7@itb.cat>
2023-12-18 13:59:24 +0100

    Activity 17 in progress

--- ACT17CelsiusToFahrenheit/Program.cs
+++ ACT17CelsiusToFahrenheit/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Utilities;
+
+namespace ACT17
+{
+    public class ACT17
+    {
+        public static void Main()
+        {
+            const string AskCelsiusTemp = "Proporcioname la temperatura en Celsius: ";
+
+            int celsius;
+
+            celsius = FunctionUtilities.AskUserValue(AskCelsiusTemp);
+        }
+
+        public static decimal CelsiusToFahrenheit(decimal celsius)
+        {
+            int amountDecimals = 2;
+            decimal conversionValue = 1.8M;
+            decimal farenheitStartingPoint = 32M;
+            decimal amountIncreases = celsius<0 ? celsius*-1 : celsius;
+            decimal result = farenheitStartingPoint;
+
+            while(Math.Truncate(amountIncreases) > 0)
+            {
+                result += celsius < 0 ? conversionValue * -1 : conversionValue;
+                amountIncreases--;
+            }
+            if(result != 0)
+            {
+                result += celsius < 0 ? (amountIncreases * conversionValue) * -1 : amountIncreases * conversionValue;
+            }
+            
+            return Math.Round(result,amountDecimals,MidpointRounding.ToZero);
+        }
+    }
+}
