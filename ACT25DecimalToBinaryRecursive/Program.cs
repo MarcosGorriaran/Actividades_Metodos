c4f4d1816801fe0f2ceb473c7b9d60b147efd895
MarcosGorriaran <Marcos.Gorriaran.7e7@itb.cat>
2024-01-09 13:58:40 +0100

    Acabado la funcion de la actividad 25 Y 26

---
+++ ACT25DecimalToBinaryRecursive/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DecimalToBinary
+{
+    public class Execute
+    {
+        public static void Main()
+        {
+            Console.WriteLine(DecimalToBinary(Convert.ToInt32(Console.ReadLine())));
+        }
+        public static string DecimalToBinary(int decimalValue)
+        {
+            return decimalValue / 2 < 2 ? $"1{decimalValue % 2}" : $"{DecimalToBinary(Convert.ToInt32(decimalValue / 2))}{decimalValue % 2}";
+        }
+    }
+}
---
+++ ACT26DigitalSumRecursive/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SumDigital
+{
+    class Execute()
+    {
+        public static void Main()
+        {
+            Console.WriteLine(GetDigitSum(24455));
+        }
+        public static int GetDigitSum(int value)
+        {
+            return value / 10 < 1 ? value : GetDigitSum(Convert.ToInt32(Math.Truncate((double)value/10)))+value%10;
+        }
+    }
+}
