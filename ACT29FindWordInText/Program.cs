79bff0d584d35ccce9998bc4f9cb7c2620dbfc6f
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-01-16 12:49:06 +0100

    Finished activity 29

---
+++ ACT29FindWordInText/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FindWordInText
+{
+    class Execute
+    {
+        public static void Main()
+        {
+            const string AskText = "Proporcioname la palabra con la que buscar y contar el caracter que desea: ";
+            const string AskWord = "Proporcioname la letra que desea buscar: ";
+            const string ShowResult = "Hay {0} '{1}' en la palabra";
+            string text;
+            char word;
+            int amountWordInText;
+
+            Console.Write(AskText);
+            text = Console.ReadLine();
+            Console.Write(AskWord);
+            word = Convert.ToChar(Console.ReadLine());
+            amountWordInText = CountWordInText(text, word);
+            Console.WriteLine(ShowResult, amountWordInText, word);
+        }
+        public static int CountWordInText(string word, char targetWord)
+        {
+            if (word.Length <= 1)
+            {
+                return word[0]==targetWord ? 1 : 0;
+            }
+            return word[0] == targetWord ? CountWordInText(word.Substring(1), targetWord) + 1 : CountWordInText(word.Substring(1), targetWord);
+        }
+    }
+}
